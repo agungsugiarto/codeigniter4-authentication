03c5e5ca9df8ca39195327493d07468d87f59cd7
Agung Sugiarto <sugiartoagung92@gmail.com>
2021-01-10 13:01:32 +0700

    initial commit

--- src/Entities/User.php
+++ src/Entities/User.php
@@ -4,13 +4,12 @@ namespace Fluent\Auth\Entities;
 
 use CodeIgniter\Entity;
 use Fluent\Auth\Config\Services;
-use Fluent\Auth\Contracts\AuthenticatableInterface;
 use Fluent\Auth\Contracts\AuthenticatorInterface;
 use Fluent\Auth\Contracts\HasAccessTokensInterface;
 use Fluent\Auth\Traits\Authenticatable;
 use Fluent\Auth\Traits\HasAccessTokens;
 
-class User extends Entity implements AuthenticatorInterface, AuthenticatableInterface, HasAccessTokensInterface
+class User extends Entity implements AuthenticatorInterface, HasAccessTokensInterface
 {
     use Authenticatable;
     use HasAccessTokens;
